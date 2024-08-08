c0e0170c279345bcfefa879aa58f3767c33d7c0a
gile987 <mladen.reljic87@gmail.com>
2024-08-08 17:35:50 +0200

    Create an interface for the register request, add props to the register action, edit the onSubmit method to dispatch the register request

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
 import { register } from "../../store/actions";
+import { RegisterRequestInterface } from "../../types/registerRequest.interface";
 @Component({
   selector: "mc-register",
   templateUrl: "./register.component.html",
@@ -24,6 +25,9 @@ export class RegisterComponent {
 
   onSubmit() {
     console.log('form: ', this.form.getRawValue());
-    this.store.dispatch(register());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(register({ request }));
   }
 }
