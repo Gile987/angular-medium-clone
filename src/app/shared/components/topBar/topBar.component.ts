ea71df42fc4d336f0ea58cd9bf4d0ff63d6762df
gile987 <mladen.reljic87@gmail.com>
2024-08-13 19:35:13 +0200

    Add a top bar component with a data observable, which has a currentUser property.

---
+++ src/app/shared/components/topBar/topBar.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from '../../../auth/store/reducers';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CurrentUserInterface } from '../../types/currentUser.interface';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mc-top-bar',
+  templateUrl: './topBar.component.html',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+})
+export class TopBarComponent {
+  data$: Observable<{
+    currentUser: CurrentUserInterface | null;
+  }>;
+
+  constructor(private store: Store) {
+    this.data$ = combineLatest({
+      currentUser: this.store.select(selectCurrentUser),
+    }).pipe(
+      map(({ currentUser }) => ({
+        currentUser: currentUser ?? null,
+      }))
+    );
+  }
+}
