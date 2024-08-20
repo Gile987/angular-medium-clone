b6cb904a644f9b77e50284346141bf690a063184
gile987 <mladen.reljic87@gmail.com>
2024-08-20 16:44:28 +0200

    Add a form group to the article form component. Add initialize form and onSubmit methods, write markup

--- src/app/shared/components/articleForm/articleForm.component.ts
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -1,16 +1,55 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { ArticleFormValuesInterface } from './types/articleFormValues.interface';
 import { BackendErrorsInterface } from '../../types/backendErrors.interface';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BackendErrorMessages } from '../backendErrorMessages/backendErrorMessages.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mc-article-form',
   templateUrl: './articleForm.component.html',
   standalone: true,
+  imports: [BackendErrorMessages, ReactiveFormsModule, CommonModule],
 })
-export class ArticleFormComponent {
-  @Input() initialValues?: ArticleFormValuesInterface;
+export class ArticleFormComponent implements OnInit {
+  @Input() initialValues: ArticleFormValuesInterface | undefined;
   @Input() isSubmitting: boolean = false;
   @Input() errors: BackendErrorsInterface | null = null;
 
   @Output() articleSubmit = new EventEmitter<ArticleFormValuesInterface>();
+
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      title: '',
+      description: '',
+      body: '',
+      tagList: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    if (!this.initialValues) {
+      throw new Error('Inputs are required');
+    }
+    this.form.patchValue({
+      title: this.initialValues.title,
+      description: this.initialValues.description,
+      body: this.initialValues.body,
+      tagList: this.initialValues.tagList.join(' '),
+    });
+  }
+
+  onSubmit(): void {
+    const formValue = this.form.getRawValue();
+    const articleFormValues: ArticleFormValuesInterface = {
+      ...formValue,
+      tagList: formValue.tagList.split(' '),
+    };
+    this.articleSubmit.emit(articleFormValues);
+  }
 }
